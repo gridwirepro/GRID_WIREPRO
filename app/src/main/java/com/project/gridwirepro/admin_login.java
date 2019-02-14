19de5a685b455dbb5d455d8a0643daba32b58bfe
ASWIN S <KHSCI5MCA16019@asas.kh.amrita.edu>
2019-02-14 13:57:22 +0530

    Initial commit

---
+++ app/src/main/java/com/project/gridwirepro/admin_login.java
@@ -0,0 +1,43 @@
+package com.project.gridwirepro;
+
+import android.app.ActivityOptions;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class admin_login extends AppCompatActivity {
+    Button login,signup;
+
+    EditText user,pass;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.admin_login);
+        user=findViewById(R.id.input_id);
+        pass=findViewById(R.id.input_pass);
+    }
+
+    public void log_click(View view)
+    {
+        Intent i = new Intent(this,ocr_page.class);
+        startActivity(i);
+
+    }
+    public void admin(View view)
+    {
+       // if((user.getText().equals("admin"))&&pass.getText().equals("admin"))
+        //{
+            Intent i = new Intent(this, admin_Activity.class);
+            startActivity(i);
+        /*}
+        else
+        {
+        Toast.makeText(this, "WRONG USER ID OR PASSWORD", Toast.LENGTH_SHORT).show();
+        }*/
+    }
+}
---
+++ app/src/main/java/com/project/gridwirepro/ocr_page.java
@@ -0,0 +1,32 @@
+package com.project.gridwirepro;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+import com.google.firebase.ml.vision.text.FirebaseVisionTextRecognizer;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class ocr_page extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ocr_page);
+    }
+}
